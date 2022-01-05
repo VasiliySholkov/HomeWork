eb6d98c2726c317d64a01dc8b0dfb934220a3320
VasiliySholkov <3730703@bk.ru>
2022-01-06 01:16:36 +0300

    MyLinkedList Dual

---
+++ src/Andersen/Main.java
@@ -0,0 +1,18 @@
+package Andersen;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyLinkedList list = new MyLinkedList();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.insert(8, 65);
+
+        list.display();
+
+    }
+}
